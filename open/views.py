3cdae76acda9b714b84c229a26b96982be1c126b
mansurkamaldinov <126574730+mansurkamaldinov@users.noreply.github.com>
2023-04-25 10:12:21 +0300

    Delete open directory

--- open/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Articles
-
-
-def open(request):
-    open = Articles.objects.all()
-    return render(request,'open/open_home.html',{"open":open})
